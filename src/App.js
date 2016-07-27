a5e963f5b59c1714852a2f12c5f23e406f0367a5
Michael D. Stemle, Jr <mstemle@cvent.com>
2016-07-26 22:29:57 -0400

    Adding some of my code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
 import React, { Component } from 'react';
+import ListView from './ListView';
 
 export default class App extends Component {
   render() {
+    const animals = [
+      { name: 'Horse', legs: 4, sound: 'winny', type: 'land', color: 'brown' },
+      { name: 'Dog',   legs: 4, sound: 'bark',  type: 'land', color: 'yellow' },
+      { name: 'Crow',  legs: 2, sound: 'caw',   type: 'bird', color: 'black' },
+    ];
+    
     return (
-      <h1>Hello, world.</h1>
+      <div>
+        <h1>Hello, world.</h1>
+        {
+        // <section>
+        // <h2>Animal Sounds</h2>
+        // <ListView
+        //   items={animals}
+        //   leftDecorator={SayNameDecorator}
+        //   itemDisplay={ColorItemDisplay}
+        //   rightDecorator={SoundDecorator} />
+        // </section>
+
+        // <section>
+        // <h2>Animal Leg Counts</h2>
+        // <ListView
+        //   items={animals}
+        //   leftDecorator={LegCountDecorator}
+        //   rightDecorator={SoundDecorator} />
+        // </section>
+        }
+        
+        <section>
+        <h2>Animal Leg Counts</h2>
+        <ListView
+          items={animals} />
+        </section>
+      </div>
     );
   }
 }
